740c04be0a60e932e70303935b44dda035978774
leonardo <las3@okibrasil.cin.ufpe.br>
2021-03-21 18:52:16 -0300

    arquivo de teste

--- matlab_code/teste.m
+++ matlab_code/teste.m
@@ -1,10 +1,12 @@
 
 
-[data_train, labels_train] = load_data('C:\Users\leona\Documents\Mestrado_code\MCSProject\ProcessedBases\Penbased\penbased_2_train.csv');
-classifiers = run_base_classifier(svdd, data_train, labels_train, 0.05, 5);
+[data_train, labels_train] = load_data('C:\Users\leona\Documents\Mestrado_code\MCSProject\ProcessedBases\Penbased\penbased_3_train.csv');
+classifiers = run_base_classifier(svdd, data_train, labels_train, 0.0045, 5);
 
-[data_test, labels_test] = load_data('C:\Users\leona\Documents\Mestrado_code\MCSProject\ProcessedBases\Penbased\penbased_2_test.csv');
+[data_test, labels_test] = load_data('C:\Users\leona\Documents\Mestrado_code\MCSProject\ProcessedBases\Penbased\penbased_3_test.csv');
 
 
-y_pred = max_agg(classifiers,data_test);
+y_pred_1 = max_agg(classifiers,data_test);
+
+y_pred = decision_templates_agg(classifiers,data_train, labels_train, data_test);
 
