058677f12d13dde648c7b7d97c13eeda179969b8
leonardo <las3@okibrasil.cin.ufpe.br>
2021-03-28 16:47:46 -0300

    corrige codigo de classificador

--- matlab_code/predict_one.m
+++ matlab_code/predict_one.m
@@ -2,17 +2,28 @@ function Y_PRED = predict_one(classifier, X_test)
    
     pr_test = prdataset(X_test);  
     pred = pr_test*classifier;
+    x = pred.FEATLAB(1,:);
     pred = pred.DATA(:,1);
+    
     qtd_instancias = size(X_test,1);
     
     Y_PRED = zeros(qtd_instancias,1);
     
     for i = 1:qtd_instancias
-        if(pred(i) >= 0.5)
-            Y_PRED(i) = 1;
+        if(x  == 'outlier')
+            if(pred(i) >= 0.5)
+                Y_PRED(i) = -1;
+            else
+                Y_PRED(i) = 1;
+            end
         else
-            Y_PRED(i) = 0;
-        end
+          if(pred(i) >= 0.5)
+                Y_PRED(i) = 1;
+            else
+                Y_PRED(i) = -1;
+          end          
+        end 
+        
     end     
     
 end
