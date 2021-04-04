69cbf39ac79baed82d974f590c29d7544bbe4fe6
Marcos Barreto <msb5@cin.ufpe.br>
2021-04-04 01:34:17 -0300

    Update experiment 3 script

--- matlab_code/run_experiment3.m
+++ matlab_code/run_experiment3.m
@@ -1,6 +1,6 @@
 function run_experiment3()
     n_folds = 5;
-    datasets = ["Auslan", "Autos", "Car", "Cleveland", "Dermatology", "Ecoli", "Flare", "Glass", "Isolet", "Led7digit", "Letter-2", "Lymphography", "Nursery", "Page-blocks", "Penbased", "Satimage", "Segment", "Shuttle", "Vehicle", "Vowel", "Yeast", "Zoo"];
+    datasets = ["Autos", "Car", "Cleveland", "Dermatology", "Ecoli", "Flare", "Glass", "Isolet", "Led7digit", "Letter-2", "Lymphography", "Nursery", "Page-blocks", "Penbased", "Satimage", "Segment", "Shuttle", "Vehicle", "Vowel", "Yeast", "Zoo", "Auslan"];
     alpha = 0.1;
     root_dataset_name = "ProcessedBases";
     result_folder = "results";
@@ -29,45 +29,64 @@ function run_experiment3()
                 [X_train, y_train] = load_data(sprintf("../%s/%s/%s_%d_train.csv", root_dataset_name, dataset_name, lower(dataset_name), n_fold));
                 [X_test, y_test] = load_data(sprintf("../%s/%s/%s_%d_test.csv", root_dataset_name, dataset_name, lower(dataset_name), n_fold));
 
-                for i = 1:length(aggregators)
-                    % use aggregator on base_classifier trained and save y_pred with a identifier
-                    aggregator_name = aggregators(i);
-                    fprintf("\t\tAggregator function: " + aggregator_name + "\n");
-                    last_aggregator_name = aggregator_name;
-                    for j = 1:length(base_classifiers)
-                        base_classifier_name = base_classifiers(j);
-                        fprintf("\t\t\tBase classifier: " + base_classifier_name + "\n");
-                        
-                        if aggregator_name == "ecoc_agg"
-                            if base_classifier_name == "svc"
-                                Parameters.base_binary = "svm";
-                            else
-                                Parameters.base_binary = base_classifier_name;
-                            end
-                        end
-                        
-                        for k = 1:length(occ_strategies)
-                            occ_strategie_name = occ_strategies(k);
-                            fprintf("\t\t\t\tOCC: " + occ_strategie_name + "\n");
+                for j = 1:length(base_classifiers)
+                    base_classifier_name = base_classifiers(j);
+                    fprintf("\t\tBase classifier: " + base_classifier_name + "\n");
+                    
+                    fprintf("\t\t\tTraining: OVO\n");
+                    base_classifier_trained_ovo = run_occ_strategy("ovo", base_classifier_name, X_train, y_train);
+                    
+                    fprintf("\t\t\tTraining: OVA\n");
+                    base_classifier_trained_ova = run_occ_strategy("ova", base_classifier_name, X_train, y_train);
+                    
+                    if base_classifier_name == "svc"
+                        Parameters.base_binary = "svm";
+                    else
+                        Parameters.base_binary = base_classifier_name;
+                    end
+                    
+                    fprintf("\t\t\tTraining: ECOC(OVO)\n");
+                    Parameters.coding = 'OneVsOne';
+                    [base_classifier_trained_ecoc_ovo, Parameters_ovo] = ECOCTrain(X_train, y_train, Parameters);
+                    
+                    fprintf("\t\t\tTraining: ECOC(OVA)\n");
+                    Parameters.coding = 'OneVsAll';
+                    [base_classifier_trained_ecoc_ova, Parameters_ova] = ECOCTrain(X_train, y_train, Parameters);
+
+                    for k = 1:length(occ_strategies)
+                        occ_strategie_name = occ_strategies(k);
+                        fprintf("\t\t\tOCC: " + occ_strategie_name + "\n");
+
+                        for i = 1:length(aggregators)
+                            % use aggregator on base_classifier trained and save y_pred with a identifier
+                            aggregator_name = aggregators(i);
+                            fprintf("\t\t\t\tAggregator function: " + aggregator_name + "\n");
+                            last_aggregator_name = aggregator_name;
                             
                             if aggregator_name == "ecoc_agg"
                                 if occ_strategie_name == "ovo"
-                                    Parameters.coding = 'OneVsOne';
-                                elseif occ_strategie_name == "ova"
-                                    Parameters.coding = 'OneVsAll';
+                                    base_classifier_trained = base_classifier_trained_ecoc_ovo;
+                                    Parameters = Parameters_ovo;
+                                else
+                                    base_classifier_trained = base_classifier_trained_ecoc_ova;
+                                    Parameters = Parameters_ova;
                                 end
-
-                                [base_classifier_trained, Parameters] = ECOCTrain(X_train, y_train, Parameters);
-                            else
-                                base_classifier_trained = run_occ_strategy(occ_strategie_name, base_classifier_name, X_train, y_train);
-                            end
-                            
-                            if occ_strategie_name == "ovo"
+                            elseif occ_strategie_name == "ovo"
                                 if aggregator_name == "max_agg"
                                     aggregator_name = "majority_agg";
                                 end
+                                if aggregator_name == "ecoc_agg"
+                                    base_classifier_trained = base_classifier_trained_ecoc_ovo;
+                                else
+                                    base_classifier_trained = base_classifier_trained_ovo;
+                                end
                             elseif occ_strategie_name == "ova"
                                 aggregator_name = last_aggregator_name;
+                                if aggregator_name == "ecoc_agg"
+                                    base_classifier_trained = base_classifier_trained_ecoc_ova;
+                                else
+                                    base_classifier_trained = base_classifier_trained_ova;
+                                end
                             end
                             
                             for l = 1:length(techniques)
